a6af91c42f3060bd13cd3ef2fea9e5abe77a1b6f
GlebKlimakhin <71899242+GlebKlimakhin@users.noreply.github.com>
2020-12-17 17:47:03 +0300

    Add files via upload
    Не считаю реализацию ExecutorService на серверной стороне правильной и абсолютно не вижу в ней плюсов.
    Когда в чате появляется клиент, для него запускается поток, по окончанию взаимодействия этот поток переходит в состояние terminate. 
    ExecutorService работает с пулом потоков. Таким образом мы ограничиваем количество клиентов, которые одновременно заходят в наш чат, этим создаём "очередь". 
    Возможно, такая реализация имеет право на существование когда чат является большим коммерческим проектом, и нам нужно рассчитывать нагрузку на сервера
    Но это просто мои мысли  и я буду рад если вы внесёте в них коррективы)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,66 @@
 package com.company;
 
+
 public class Main {
 
+    private static volatile int counter = 1;
+    private static final Object obj = new Object();
+    private static final int number= 5;
+    /*1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз
+       (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+        */
     public static void main(String[] args) {
-	// write your code here
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < number; i++) {
+                synchronized (obj) {
+                    while (counter != 1) {
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print("A ");
+                    counter = 2;
+                    obj.notifyAll();
+                }
+            }
+        });
+        t1.start();
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < number; i++) {
+                synchronized (obj){
+                    while (counter != 2){
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print("B ");
+                    counter = 3;
+                    obj.notifyAll();
+                }
+            }
+        });
+        t2.start();
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < number; i++) {
+                synchronized (obj){
+                    while (counter != 3){
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print("С ");
+                    counter = 1;
+                    obj.notifyAll();
+                }
+            }
+        });
+        t3.start();
+
     }
 }
