8b3b01d0971ed327a78d3b40b790fc9c9c31405d
GlebKlimakhin <71899242+GlebKlimakhin@users.noreply.github.com>
2021-01-12 12:29:29 +0300

    Add files via upload

---
+++ src/com/company/Employee.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Employee {
+    private String fio;
+    private String position;
+    private String email;
+    private String tel;
+    private int salary;
+    private int age;
+
+
+    public Employee(String fio, String position, String email, String tel, int salary, int age){
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.tel = tel;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void info() {
+        System.out.println("fio: " + this.getFio() + "; Должность: " + this.getPosition()+ "; Почта: " +
+                this.getEmail() + "; Телефон: " + this.getTel() + "; Зарплата: " + this.getSalary());
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Ivanov Ivan Ivanovich", "SEO", "ivanov@class.com", "94756", 100000, 42);
+        persArray[1] = new Employee("Petrov Petr Petrovich", "Account", "petrov@class.com", "34432", 50000, 30);
+        persArray[2] = new Employee("Hudo Gavrila Sanich ", "Finance", "hudo@class.com", "43333", 90000, 45);
+        persArray[3] = new Employee("Puzo Ostap Gavrilich ", "Group head", "puzo@class.com", "44444", 45000, 29);
+        persArray[4] = new Employee("Kapusta Lev Durindich", "engineer", "kapusta@class.com", "12312", 47000, 28);
+
+
+        for (int i = 0; i < persArray.length; i++) {
+            if (persArray[i].getAge() > 40) {
+                System.out.println(persArray[i].getFio() + " Сотрудник старше 40 лет");
+                persArray[i].info();
+
+            }
+        }
+
     }
 }
